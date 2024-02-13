7c3df40ed04ae4f36e76f068470f1d054b5a8d59
Prince Bhatt <prince25io044@satiengg.in>
2024-02-13 22:30:53 +0530

    Website Created

---
+++ script.js
@@ -0,0 +1,10 @@
+var swiper = new Swiper('.mySwiper', {
+  effect: 'cards',
+  grabCursor: true,
+});
+function nextPage() {
+  window.location.href = 'yes.html';
+}
+function nextPage2() {
+  window.location.href = 'prom.html';
+}
