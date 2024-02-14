93768cde92f369ff3e6d4c92c83edc1dd45ac4b5
Prince Bhatt <prince25io044@satiengg.in>
2024-02-14 09:15:52 +0530

    Website Updated

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
-var swiper = new Swiper('.mySwiper', {
-  effect: 'cards',
-  grabCursor: true,
-});
 function nextPage() {
   window.location.href = 'yes.html';
 }
 function nextPage2() {
   window.location.href = 'prom.html';
+}
+function nextPage3() {
+  window.location.href = 'spec.html';
 }
